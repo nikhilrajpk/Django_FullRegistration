a15fc14c2813338e06b92da78a1d945777d75055
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-09-12 13:50:34 +0530

    model form done

--- fullReg_project/app1/urls.py
+++ fullReg_project/app1/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('',views.signupPage,name='signupPage'),
     path('login/',views.loginPage,name='loginPage'),
     path('home/',views.homePage,name='homePage'),
+    path('logout/',views.logoutPage,name='logoutPage')
 ]
--- fullReg_project/app1/views.py
+++ fullReg_project/app1/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import login,logout
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 # Create your views here.
+@never_cache
 def signupPage(request):
-    return render(request,'signup.html')
+    # if request.method == 'POST':
+    #     form = CustomForm(request.POST)
+    #     if form.is_valid:
+    #         form.save()
+    #         messages.success(request,'Your account has been created ')
+    #         return redirect(loginPage)
+    # else:
+    #     form = CustomForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created!')
+            return redirect('loginPage')  # Redirect to login page after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request,'signup.html',{'form':form})
 
+@never_cache
 def loginPage(request):
+    if request.method == 'POST':
+        pass
     return render(request,'login.html')
 
+@never_cache
 def homePage(request):
     return render(request,'home.html')
+
+@never_cache
+def logoutPage(request):
+    return redirect(loginPage)
